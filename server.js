403ebc1263f5b59285930e5190f9dae5b5fba682
root <root@apps.smuzzies.com>
2024-04-11 19:25:28 -0500

    cleanup

--- server.js
+++ server.js
@@ -17,22 +17,19 @@ const options = {
 
 app.use(express.static('public'));
 
-app.get('/api/blockinfo', async (req, res) => {
+app.get('/api/data', async (req, res) => {
+  const currentDate = new Date().toISOString().split('T')[0];
+  const url = `https://api.vechainstats.com/v2/block/stats?date=${currentDate}&expanded=true&VCS_API_KEY=${process.env.API_KEY}`;
+
   try {
-    const blockNumber = req.query.blocknum;
-
-    console.log(`Fetching block information for block number ${blockNumber}`);
-
-    const response = await axios.get(`https://api.vechainstats.com/v2/block/info?blocknum=${blockNumber}&VCS_API_KEY=${apiKey}`);
-
-    console.log('Block information fetched successfully');
-    console.log('Response data:', response.data);
-    res.json(response.data);
+    const apiResponse = await axios.get(url);
+    res.json(apiResponse.data);
   } catch (error) {
-    console.error('Error fetching block information:', error);
-    res.status(500).json({ error: 'Error fetching block information' });
+    console.error('Failed to fetch API data:', error);
+    res.status(500).json({ error: 'Failed to fetch API data' });
   }
 });
+
 
 const server = https.createServer(options, app);
 
